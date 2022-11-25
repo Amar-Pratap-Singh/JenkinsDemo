b64966c8f28e57ab9cfac9a0d56c367191746b0c
Amar Pratap Singh <75470134+Amar-Pratap-Singh@users.noreply.github.com>
2022-11-25 11:55:46 +0530

    Update testCase3.py

--- testCase3.py
+++ testCase3.py
@@ -7,7 +7,7 @@ from multiplication import multiply
 class TestCase(unittest.TestCase):
     def testCase3(self):
         result = multiply(1, 7)
-        self.assertEqual(result, 3)
+        self.assertEqual(result, 7)
 
 
 if __name__ == '__main__':
